537aa93849f8e5424914724b2bafbc58a3327777
Anton Lipatov <tonybass1414@yahoo.com>
2024-08-17 22:18:09 +0500

    fix seed.py

--- database/config/seed.py
+++ database/config/seed.py
@@ -9,10 +9,11 @@ if len(sys.argv) < 2:
     print("Seed command: seed <seeder_class or all>")
     sys.exit(1)
 else:
-    seeder = sys.argv[1]
-    if seeder == 'all':
+    seeder_name = sys.argv[1]
+    if seeder_name == 'all':
         seeders_list = [cls for name, cls in globals().items() if inspect.isclass(cls)]
         for seeder in seeders_list:
             seeder()
     else:
+        seeder = globals().get(seeder_name)
         seeder()
