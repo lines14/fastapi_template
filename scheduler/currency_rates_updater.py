9826719bf3505c5d2ac13646c518e69a62e31d1c
Anton Lipatov <tonybass1414@yahoo.com>
2024-08-30 20:52:42 +0500

    up

--- scheduler/currency_rates_updater.py
+++ scheduler/currency_rates_updater.py
@@ -24,7 +24,7 @@ class CurrencyRatesUpdater:
         currency_rates = list(filter(lambda currency_rate: currency_rate.title in titles, currency_rates))
         currency_rates = sorted(currency_rates, key=lambda currency_rate: titles.index(currency_rate.title))
         currency_rates_models = list(map(lambda currency_rate: CurrencyRate(currency_id=(await Currency(currency=currency_rate.title)
-                                                                            .get()).id, rate=currency_rate.rate), currency_rates))
+                                                                                         .get()).id, rate=currency_rate.rate), currency_rates))
         await Database().seed([
             CurrencyRate(currency_id=(await Currency(currency='KZT').get()).id, rate=1),
             *currency_rates_models
